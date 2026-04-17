1f22914249da4e9418b4a372f5dfe6d3584a02fa
z0d1akK <artemlinenko10@gmail.com>
2026-04-17 17:28:38 +0300

    check ci/cd yan

--- src/main/java/com/atbp/laba3/FareCalculator.java
+++ src/main/java/com/atbp/laba3/FareCalculator.java
@@ -11,7 +11,6 @@ public class FareCalculator {
         this.cardService = cardService;
     }
 
-
     public double calculateFare(int zones, String ticketType, String cardId) {
         CardStatus status = cardService.checkStatus(cardId);
         if (status == CardStatus.BLOCKED) {
